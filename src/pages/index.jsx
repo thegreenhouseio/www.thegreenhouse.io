2532d7fcb3ecf2ed64a2a0758ea8c3211467d8ec
Owen Buckley <owenbuckley13@gmail.com>
2018-03-17 15:54:27 -0400

    poc pages and components

---
+++ src/pages/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+export default () => {
+  return (
+    <div style={{ color: 'red' }}>
+      <h1>Hello World!</h1>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    
+      <br/>
+
+      <div>
+        <Link to="/page-2/">Page 2</Link>
+      </div>
+
+
+      <div>
+        <Link to="/counter/">Counter</Link>
+      </div>
+    </div>
+
+  )
+};
