2e03c7b9decf992432f45c9bea9c37737c64d52b
Owen Buckley <owen@thegreenhouse.io>
2018-03-19 14:59:08 -0400

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,25 @@ module.exports = {
   siteMetadata: {
     title: 'The Greenhouse I/O - Building Your Future Together 👐 🌱 💻'
   },
-  plugins: ['gatsby-plugin-typography']
+  plugins: [
+    'gatsby-plugin-typography', 
+    {
+      resolve:  'gatsby-plugin-favicon',
+      options: {
+        logo: './src/favicon.jpg',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    }
+  ]
 }
