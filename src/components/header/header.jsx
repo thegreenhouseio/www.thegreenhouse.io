88c1ad5073b3d444942728476061161fd4d459d9
Owen Buckley <owen@thegreenhouse.io>
2018-03-18 22:07:42 -0400

    improving reability / functionalty of the home page (#12)

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Link from 'gatsby-link';
 import './header.css';
 
 const Header = () => {
   return (
     <div> 
-      <header></header>
-      <h2 className="caption">A DREAMER BY DESIGN</h2>
+      <Link to="/">
+        <header></header>
+        <h2 className="caption">A DREAMER BY DESIGN</h2>
+      </Link>
     </div>
   )
 };
--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -6,10 +6,10 @@ const Naviagation = () => {
   return (
     <nav>
       <ul>
-        <li><Link to="/about/">About</Link></li>
-        <li><Link to="/projects/">Projects</Link></li>
+        {/* <li><Link to="/writing/">About</Link></li> */}
+        {/* <li><Link to="/projects/">Projects</Link></li>
         <li><Link to="/blog/">Blog</Link></li>
-        <li><Link to="/contact/">Contact</Link></li>
+        <li><Link to="/contact/">Contact</Link></li> */}
       </ul>
     </nav>
   )
