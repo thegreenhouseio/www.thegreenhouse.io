3fa83b6aafe630473d8550b9afc939e068c0f97f
Owen Buckley <owen@thegreenhouse.io>
2018-03-19 20:57:53 -0400

    nice styles

--- src/services/presentations-service.js
+++ src/services/presentations-service.js
@@ -3,24 +3,24 @@ class PresentationsService {
     // add new presentations at the top, i.e. FIFO
     this.presentations = [{
       title: 'Providence Geeks 2.0: Building a Website and a Community',
-      abstract: 'Part two in my performance series for the Providence Geeks website.',
+      abstract: 'A meetup I did at the Kenzan Meetup on the work done to rebuild the website for the local community meetup group Providence Geeks.  Covers the entire process from design to delivery and everything in between.',
       link: 'https://www.meetup.com/Kenzan-Providence-Hack-Nights/events/246620015/',
       date: '1/18/2018',
       video: 'https://www.youtube.com/embed/_23QLTJ-eIw',
     }, {
       title: 'A JavaScript Stack to Build On: Intersecting UX and DX',
-      abstract: 'ProvJS Meetup presentation I gave on my article series for Kenzan',
+      abstract: 'ProvJS Meetup presentation I gave on my article series for Kenzan, covering the topic of Yarn, webpack and TypeScript.  Included a code walkthrough on a webpack project.',
       link: 'https://www.meetup.com/Prov-JS/events/241787664/',
       date: '8/3/2017'
     }, {
       title: 'JavaScript in 2017',
-      abstract: 'Kenzan Meetup Presentation on the current state of JavaScript',
+      abstract: 'Kenzan Meetup Presentation on the current state of JavaScript in 2017.',
       link: 'https://www.meetup.com/Kenzan-Providence-Hack-Nights/events/237147005/',
       date: '8/3/2017', 
       video: 'https://www.youtube.com/embed/HlHTaBUmfUQ',
     }, {
       title: 'Managing an Open Source Project in GitHub',
-      abstract: 'Presentation I gave at the Linux Open Source Leadership Summit for Kenzan',
+      abstract: 'Presentation I gave at the Linux Open Source Leadership Summit for Kenzan for tips and features to use in GitHub for managing an open source project in GitHub.',
       link: 'http://events.linuxfoundation.org/events/open-source-leadership-summit/program/schedule',
       date: '3/16/2017'
     }]
--- src/services/projects-service.js
+++ src/services/projects-service.js
@@ -2,25 +2,25 @@ class ProjectsService {
   constructor() {
     // add new projects at the top, i.e. FIFO
     this.projects = [{
-      name: 'Health Of Mind Enhanced',
-      description: 'Co-working and collaboration space I co-own in Newport, RI.',
+      name: 'Health Of Mind Enhanced (HOME)',
+      description: 'Co-working and collaboration space I co-own in Newport, RI focused on providing an open space to work study, and collobrate with health conscious work station otions like bike desks and standing desks.  A great place to work from HOME!',
       link: 'https://healthofmindenhanced.com/'
     },{
       name: 'Providence Geeks Website',
-      description: 'Open source website project for the local tech meetup community.',
+      description: 'Open source website project for the local tech meetup community that I helped lead for Kenzan and continue to maintain and help develop.  Currently working on making the site performant and consideration for becoming a Progressive Web Application (PWA).',
       link: 'https://github.com/ProvidenceGeeks',
       img: 'https://cdn-images-1.medium.com/max/2000/1*Itn_4AfT13qgCN-YcsuMsQ.png'
     }, {
       name: 'Newport Data Project',
-      description: 'A local community group bridging technology and civic action.',
+      description: 'A local community group bridging technology and civic action by finding and consuming open data sets to address challenges right here in Newport like roads and crosswalks, sea rise, and pedestrian safety.',
       link: 'http://newportdataproject.org/'
     }, {
       name: 'GitHub Dashboard',
-      description: 'A local webapp I buitl to manage your issues and PRs in GitHub.',
+      description: 'A local webapp I built to manage your issues and PRs in GitHub.  It allows you to track issues and PRs assigned to you across all your repositories, both personal and subscribed.',
       link: 'https://github.com/thescientist13/github-dashboard/'
     }, {
       name: 'Analog Studios',
-      description: 'Local home music studio I co-run in Newport, RI.',
+      description: 'Local home music studio I co-run in Newport, RI with my friend.  A great place to come jam or lay down some tracks, but always keepin it reel.',
       link: 'https://www.analogstudios.net',
       img: 'https://s3.amazonaws.com/uploads.thegreenhouse.io/website/analogstudios-banner.png'
     }]
