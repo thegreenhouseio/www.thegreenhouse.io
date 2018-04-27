1091a96e2a93c035a4061265e6bc00555af882ea
Owen Buckley <owen@thegreenhouse.io>
2018-04-27 14:52:42 -0400

    PWA recommendations WIP

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,21 +12,15 @@ module.exports = {
         trackingId: 'UA-117350131-1'
       }
     }, {
-      resolve: 'gatsby-plugin-favicon',
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        logo: './src/favicon.jpg',
-        injectHTML: true,
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          twitter: false,
-          yandex: false,
-          windows: false
-        }
+        name: 'Owen Buckley - The Greenhouse I/O',
+        short_name: 'tgh.io', // eslint-disable-line camelcase
+        start_url: '/', // eslint-disable-line camelcase
+        background_color: '#007acc', // eslint-disable-line camelcase
+        theme_color: '#efefef', // eslint-disable-line camelcase
+        display: 'minimal-ui',
+        icon: 'src/favicon.jpg' // This path is relative to the root of the site.
       }
     }
   ]
