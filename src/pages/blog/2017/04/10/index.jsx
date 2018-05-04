580418048ad9ca144839d3a39f812aec144e1468
Owen Buckley <owen@thegreenhouse.io>
2018-05-04 12:01:33 -0400

    only use noopener

--- src/pages/blog/2017/04/10/index.jsx
+++ src/pages/blog/2017/04/10/index.jsx
@@ -15,12 +15,12 @@ class BlogPostPage extends React.Component {
         <div>
           <p>The Greenhouse is happy to announce the 1.0.0 release of GitHub Dashboard!  Since the last RC, the focus was on unit testing, 
           getting code coverage up (now at 93%!), and eliminating technical debt.  For more information, please review the 
-          <OutboundLink href="https://github.com/thescientist13/github-dashboard/releases/tag/1.0.0" target="_blank" rel="noopener noreferrer"> release notes.</OutboundLink></p>
+          <OutboundLink href="https://github.com/thescientist13/github-dashboard/releases/tag/1.0.0" target="_blank" rel="noopener"> release notes.</OutboundLink></p>
 
-          <p>You can see what upcoming features are being planned next in the <OutboundLink href="https://github.com/thescientist13/github-dashboard/projects/4" target="_blank" rel="noopener noreferrer">Project tracker.</OutboundLink></p>
+          <p>You can see what upcoming features are being planned next in the <OutboundLink href="https://github.com/thescientist13/github-dashboard/projects/4" target="_blank" rel="noopener">Project tracker.</OutboundLink></p>
 
           <p>Please feel free to leave thoughts and feedback, bug reports, or feature requests by using the 
-            <OutboundLink href="https://github.com/thescientist13/github-dashboard/issues" target="_blank" rel="noopener noreferrer"> issue tracker.</OutboundLink></p>
+            <OutboundLink href="https://github.com/thescientist13/github-dashboard/issues" target="_blank" rel="noopener"> issue tracker.</OutboundLink></p>
         </div>
       </BlogPost>
     );
