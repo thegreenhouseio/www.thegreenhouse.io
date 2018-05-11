4e939fa8a0e4fa4a31dab4bab6bc1053fba3e6d9
Owen Buckley <owen@thegreenhouse.io>
2018-05-11 14:21:17 -0400

    added gatsby helmet plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-typography', 
     'gatsby-plugin-sitemap',
+    'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
