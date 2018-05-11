a07300462696b92b5083d6ba9425991cd9ea8a95
Owen Buckley <owen@thegreenhouse.io>
2018-05-11 15:10:30 -0400

    dynamic blog post content being statically generatedgit status

--- src/components/blog-post/blog-post.jsx
+++ src/components/blog-post/blog-post.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import './blog-post.css';
 
-const BlogPost = (props) => {
+function slugifyDate(date) {
+  const dateArray = date.split('.');
 
+  return `${dateArray[2]}/${dateArray[1]}/${dateArray[0]}/`;
+}
+
+const BlogPost = (props) => {
+  // <meta> data intentionally set to reference production
+  const canonicalUrl = `https://www.thegreenhouse.io/blog/${slugifyDate(props.date)}`;
+  const canonicalImageUrl = `https://s3.amazonaws.com/www.thegreenhouse.io/static${props.image}`;
   const headerBackgroundStyle = {
     backgroundImage: `url('${props.image}')`
   };
 
   return (
-    <div className="blog-post">
+    <div>
+      <Helmet>
+        <meta property="og:title" content="The Greenhouse I/O - Blog" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content={canonicalUrl}/>
+        <meta property="og:image" content={canonicalImageUrl} />
+        <meta property="og:description" content={props.title} />
+      </Helmet>
 
-      <div className="header" style={headerBackgroundStyle}>
-        <h1 className="title">{props.title}</h1>
-        <h5 className="date">Published: {props.date}</h5>
-      </div>
+      <div className="blog-post">
 
-      <div className="content">
-        { props.children }
+        <div className="header" style={headerBackgroundStyle}>
+          <h1 className="title">{props.title}</h1>
+          <h5 className="date">Published: {props.date}</h5>
+        </div>
+
+        <div className="content">
+          { props.children }
+        </div>
+      
       </div>
-    
     </div>
   );
 };
