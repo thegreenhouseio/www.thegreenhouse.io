10deb2353fa80fac3fd37eac75ebe4a64edfd11f
Owen Buckley <owen@thegreenhouse.io>
2018-05-15 10:04:44 -0400

    fix copy and links on home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,28 +7,28 @@ class HomePage extends React.Component {
     super();
     
     this.state = {
-      socialLinksMap: new SocialLinksService().getLinks(true)
+      socialLinksMap: new SocialLinksService().getLinksAsMap()
     };
+
+    console.log('state', this.state);
   }
   
   render() {
     return (  
       <div id="home">      
         <p>Hello, I&apos;m <b>Owen</b>! 👋</p>
-          
-        <p>I consider myself a problem solver and a passionate lifelong learner specializing in web 
-          application architectures and business / product development.  My broad range of technical knowledge and capabilities, 
-          creativity, interpersonal skills, and a good splash of entrepreneurial spirit make me a valuable contributor to all stages of the
-          software delivery lifecycle (SDLC) and business operations. I consider the hallmarks of any successful endeavor to 
-          include strong communication and feedback to ensure expectations are not just met, but exceeded.</p>
 
-        <p><b>The Greenhouse</b> is my platform for growing and building ideas, projects, and businesses with pragmatic technology 
-          solutions leveraging some of the best tools available today. Projects are designed and developed to be 
-          performant and maintainable (developer experience), automated in their delivery (CI / CD) and most importantly,
-          enable great user experiences (UX).</p>
+        <p>I find myself most passionate about helping people learn more about technology and how it can help them make their ideas a reality.  I like to do so through 
+          software projects that are well designed, tested, performant, and maintained in a way that advocates for both user <i>and</i> developer experiences.  My broad range 
+          of technical knowledge and capabilities, creativity, interpersonal skills, and a good splash of entrepreneurial spirit make me a valuable contributor to any project.</p>
 
-        <p>I&apos;m always up to something online be it on <OutboundLink href={this.state.socialLinksMap.twitter} target="_blank" rel="noopener">twitter</OutboundLink>, 
-          <OutboundLink href={this.state.socialLinksMap.medium} target="_blank" rel="noopener">Medium</OutboundLink>, 
+        <p><b>The Greenhouse</b> is my platform for growing and building ideas, projects, and businesses with pragmatic technology solutions leveraging some of the best tools 
+          available today. Projects are designed and developed to be performant and maintainable, automated in their delivery (CI / CD) and most importantly, enable great 
+          user experiences (UX).  Simplicity and documentation are key to ensuring accessability to all the implementation details of 
+          our <OutboundLink href={this.state.socialLinksMap.github} target="_blank" rel="noopener">shared GitHub repositories.</OutboundLink></p>
+
+        <p>I&apos;m always up to something online be it on <OutboundLink href={this.state.socialLinksMap.twitter} target="_blank" rel="noopener">twitter</OutboundLink>
+          , <OutboundLink href={this.state.socialLinksMap.medium} target="_blank" rel="noopener">Medium</OutboundLink>, 
           or around the local <OutboundLink href="https://www.pvdgeeks.org" target="_blank" rel="noopener">Rhode Island Tech Meetup Community</OutboundLink>, so I hope to hear from you!</p>
 
         <span><i>I want to help build your future.</i></span>
