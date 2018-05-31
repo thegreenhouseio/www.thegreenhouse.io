812324b2a874d9547e253658866abbd0f4201155
Owen Buckley <owen@thegreenhouse.io>
2018-05-31 10:43:48 -0400

    project evergreen content

--- src/services/projects/projects-service.js
+++ src/services/projects/projects-service.js
@@ -2,11 +2,17 @@ class ProjectsService {
   constructor() {
     // add new projects at the top, i.e. FIFO
     this.projects = [{
-      name: 'Health Of Mind Enhanced (HOME)',
-      description: `Co-working space I co-own in Newport, RI focused on providing an open space to work, study, and collobrate in a health conscious work environment
-                    featuring standing desks and bike desks, all in a modern art gallery setting.  A great place to work from HOME!`,
-      link: 'https://healthofmindenhanced.com/'
+      name: 'Project Evergreen',
+      description: `Project Evergreen is an effort I started to provide clear, detailed, and practical resources to those looking to build 
+                    modern, dynamic, performant, and evergreen web applications.`,
+      link: 'https://project-evergreen.thegreenhouse.io/',
+      img: 'https://s3.amazonaws.com/uploads.thegreenhouse.io/project-evergreen/logo-small.png'
     }, {
+    //   name: 'Health Of Mind Enhanced (HOME)',
+    //   description: `Co-working space I co-own in Newport, RI focused on providing an open space to work, study, and collobrate in a health conscious work environment
+    //                 featuring standing desks and bike desks, all in a modern art gallery setting.  A great place to work from HOME!`,
+    //   link: 'https://healthofmindenhanced.com/'
+    // }, {
       name: 'Providence Geeks Website',
       description: `Open source website project for the local tech meetup community Providence Geeks, that I helped lead for Kenzan and continue to maintain and help develop.  
                     Currently working on making the site performant and consideration for becoming a Progressive Web Application (PWA).`,
@@ -20,7 +26,8 @@ class ProjectsService {
     }, {
       name: 'GitHub Dashboard',
       description: 'A local webapp I built to manage your issues and PRs in GitHub.  It allows you to track issues and PRs assigned to you across all your repositories, both personal and subscribed.',
-      link: 'https://github.com/thescientist13/github-dashboard/'
+      link: 'https://github.com/thescientist13/github-dashboard/',
+      img: 'https://s3.amazonaws.com/uploads.thegreenhouse.io/oss/github-dashboard-v1.2.0.png'
     }, {
       name: 'Analog Studios',
       description: 'Local home music studio I co-run in Newport, RI with my friend.  A great place to come jam or lay down some tracks, but always keepin it reel.',
