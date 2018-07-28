3d3f2b09c674e1253d943370bf4ec563fb54a9bf
Owen Buckley <owenbuckley13@gmail.com>
2018-07-28 16:38:58 -0400

    expanded header and nav tests

--- src/components/header/header.spec.jsx
+++ src/components/header/header.spec.jsx
@@ -26,4 +26,22 @@ describe('Header Component', () => {
     expect(header.find('.header').length).toEqual(1);
   });
 
+  it('should have a <header> element', () => {
+    expect(header.find('header').length).toEqual(1);
+  });
+
+  it('should have <h2> element with text', () => {
+    const heading = header.find('h2');
+
+    expect(heading.length).toEqual(1);
+    expect(heading.text()).toEqual('A DREAMER BY DESIGN');
+  });
+
+  it('should have a link to the home page', () => {
+    const link = header.find('Link');
+
+    expect(link.length).toEqual(1);
+    expect(link.prop('to').pathname).toEqual('/');
+  });
+
 });
--- src/components/navigation/navigation.spec.jsx
+++ src/components/navigation/navigation.spec.jsx
@@ -7,14 +7,15 @@ import Navigation from './navigation';
 configure({ adapter: new Adapter() });
 
 describe('Navigation Component', () => {
-  let header;
+  const navLength = 3;
+  let navigation;
 
   global.___loader = {
     enqueue: jest.fn()
   };
   
   beforeEach(() => {
-    header = mount(
+    navigation = mount(
       <MemoryRouter>
         <Navigation/>
       </MemoryRouter>
@@ -22,8 +23,89 @@ describe('Navigation Component', () => {
   });
 
   it('should not be null', () => {
-    expect(header).not.toBeNull();
-    expect(header.find('.navigation').length).toEqual(1);
+    expect(navigation).not.toBeNull();
+    expect(navigation.find('.navigation').length).toEqual(1);
+  });
+
+  it('should have a <nav> elements', () => {
+    expect(navigation).not.toBeNull();
+    expect(navigation.find('nav').length).toEqual(1);
+  });
+
+  it('should have an ul with three items', () => {
+    expect(navigation.find('ul').length).toEqual(1);
+    expect(navigation.find('ul li').length).toEqual(3);
+  });
+
+  it('should have three <Link> components', () => {
+    expect(navigation.find('Link').length).toEqual(navLength);
+  });
+
+  it('should have three <Link> components', () => {
+    expect(navigation.find('Link').length).toEqual(navLength);
+  });
+
+  describe('About link', () => {
+    let aboutLink;
+
+    beforeEach(() => {
+      aboutLink = aboutLink = navigation.find('ul li Link#link-about');
+    });
+
+    it('should only have one About link', () => {
+      expect(aboutLink.length).toBe(1);
+    });
+
+    it('<Link> component should say About', () => {
+      expect(aboutLink.text()).toEqual('About');
+    });
+
+    it('should link to the About page', () => {
+      expect(aboutLink.prop('to').pathname).toEqual('/about/');
+    });
+
+  });
+
+  describe('Projects link', () => {
+    let projectsLink;
+
+    beforeEach(() => {
+      projectsLink = navigation.find('ul li Link#link-projects');
+    });
+
+    it('should only have one Projects link', () => {
+      expect(projectsLink.length).toBe(1);
+    });
+
+    it('<Link> component should say Projects', () => {
+      expect(projectsLink.text()).toEqual('Projects');
+    });
+
+    it('should link to the About page', () => {
+      expect(projectsLink.prop('to').pathname).toEqual('/projects/');
+    });
+    
+  });
+
+  describe('Blog link', () => {
+    let blogLink;
+
+    beforeEach(() => {
+      blogLink = blogLink = navigation.find('ul li Link#link-blog');
+    });
+
+    it('should only have one Blog link', () => {
+      expect(blogLink.length).toBe(1);
+    });
+
+    it('<Link> component should say Blog', () => {
+      expect(blogLink.text()).toEqual('Blog');
+    });
+
+    it('should link to the Blog page', () => {
+      expect(blogLink.prop('to').pathname).toEqual('/blog/');
+    });
+    
   });
 
 });
