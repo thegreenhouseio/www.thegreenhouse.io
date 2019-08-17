6f5cc16c7ccaa52b8e725c872f011ccc9e771144
Owen Buckley <owenbuckley13@gmail.com>
2019-08-17 17:37:19 -0400

    social icon links

--- src/components/social-icon-link/social-icon-link.js
+++ src/components/social-icon-link/social-icon-link.js
@@ -21,16 +21,10 @@ class SocialIconLinkComponent extends LitElement {
 
   render() {
     const { link, name } = this;
-
-    console.log('link', link);
-    console.log('name', name);
-
     let detectedName = '';
     let detectedLink = '';
 
     if (link && !name) {
-      console.log('IS MEETUP????', link.indexOf('meetup') >= 0);
-
       if (link.indexOf('meetup') >= 0) {
         detectedName = 'meetup';
       } else if (link.indexOf('medium') >= 0) {
@@ -38,76 +32,28 @@ class SocialIconLinkComponent extends LitElement {
       } else {
         detectedName = 'default';
       }
-      // switch (link) {
-
-      //   case link.indexOf('github') >= 0:
-      //     console.log('picking github!!!!!!!!!!');
-      //     detectedName = 'github';
-      //     break;
-      //   case link.indexOf('linkedin') >= 0:
-      //     console.log('picking linkedin!!!!!!!!!!');
-      //     detectedName = 'linkedin';
-      //     break;
-      //   case link.indexOf('medium') >= 0:
-      //     console.log('picking medium!!!!!!!!!!');
-      //     detectedName = 'medium';
-      //     break;
-      //   case link.indexOf('meetup') >= 0:
-      //     console.log('picking meetup!!!!!!!!!!');
-      //     detectedName = 'meetup';
-      //     break;
-      //   case link.indexOf('twitter') >= 0:
-      //     console.log('picking twitter!!!!!!!!!!');
-      //     detectedName = 'twitter';
-      //     break;
-      //   default:
-      //     console.log('NOT picking anything!!!!!!!!!!');
-      //     detectedName = 'default';
-      //     break;
-          
-      // }
     } else if (name) {
-      console.log('name else???');
       detectedName = name;
     }
 
     if (name && !link) {
-      detectedLink = new SocialLinksService().getLinksAsMap()[name].link;
+      detectedLink = new SocialLinksService().getLinksAsMap().get(name);
     } else if (link) {
       detectedLink = link;
     }
 
-    console.log('detectedName', detectedName);
-    console.log('detectedLink', detectedLink);
-
-    const logoPath = `/assets/logos/${detectedName}.svg`;
-
-    console.log('logoPath', logoPath);
-    console.log('********************');
-
     return html`
       <style>
-        ul {
-          list-style: none;
-          padding: 0;
-          margin-top: 10px;
-        }
-
-        li {
-          display: inline-block;
-        }
-
         img {
           display: block;
           height: 60px;
           width: 60px;
           margin: 0 10px;
         }
-        
       </style>
 
       <a rel="noopener" target="_blank" href="${detectedLink}">
-        <img src="${logoPath}">
+        <img src="/assets/logos/${detectedName}.svg">
       </a>
     `;
   }
