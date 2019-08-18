33794213596d545dca19ebed347b3bd5c218d7ac
Owen Buckley <owenbuckley13@gmail.com>
2019-08-18 15:43:02 -0400

    social icon link specs

--- src/components/social-icon-link/social-icon-link.spec.js
+++ src/components/social-icon-link/social-icon-link.spec.js
@@ -50,4 +50,133 @@ describe('SocialIconLink Component', () => {
     });
   });
 
+  describe('GitHub link', () => {
+    const mockLink = 'http://www.github.com/this';
+    let img = '';
+    let link = '';
+
+    beforeEach(async () => {
+      socialIconLink = document.createElement('app-social-icon-link');
+      socialIconLink.link = mockLink;
+
+      document.body.appendChild(socialIconLink);
+  
+      await socialIconLink.updateComplete;
+
+      img = socialIconLink.shadowRoot.querySelectorAll('a img');
+      link = socialIconLink.shadowRoot.querySelectorAll('a');
+    });
+
+    it('should have one <a> tag', () => {
+      expect(link.length).toBe(1);
+    });
+
+    it('should have default <a> tag href', () => {
+      expect(link[0].href).toContain(mockLink);
+    });
+
+    it('should have default <a> tag rel', () => {
+      expect(link[0].rel).toBe('noopener');
+    });
+
+    it('should have default <a> tag target', () => {
+      expect(link[0].target).toBe('_blank');
+    });
+
+    it('should have one <img> tag inside the <a> tag', () => {
+      expect(img.length).toBe(1);
+    });
+
+    it('should have default <img> tag with src', () => {
+      expect(img[0].src).toContain('/assets/logos/github.svg');
+    });
+
+  });
+
+  describe('Meetup link', () => {
+    const mockLink = 'http://www.meetup.com/that';
+    let img = '';
+    let link = '';
+
+    beforeEach(async () => {
+      socialIconLink = document.createElement('app-social-icon-link');
+      socialIconLink.link = mockLink;
+
+      document.body.appendChild(socialIconLink);
+  
+      await socialIconLink.updateComplete;
+
+      img = socialIconLink.shadowRoot.querySelectorAll('a img');
+      link = socialIconLink.shadowRoot.querySelectorAll('a');
+    });
+
+    it('should have one <a> tag', () => {
+      expect(link.length).toBe(1);
+    });
+
+    it('should have default <a> tag href', () => {
+      expect(link[0].href).toContain(mockLink);
+    });
+
+    it('should have default <a> tag rel', () => {
+      expect(link[0].rel).toBe('noopener');
+    });
+
+    it('should have default <a> tag target', () => {
+      expect(link[0].target).toBe('_blank');
+    });
+
+    it('should have one <img> tag inside the <a> tag', () => {
+      expect(img.length).toBe(1);
+    });
+
+    it('should have default <img> tag with src', () => {
+      expect(img[0].src).toContain('/assets/logos/meetup.svg');
+    });
+
+  });
+
+  describe('Medium link', () => {
+    const mockLink = 'http://www.medium.com/other';
+    let img = '';
+    let link = '';
+
+    beforeEach(async () => {
+      socialIconLink = document.createElement('app-social-icon-link');
+      socialIconLink.link = mockLink;
+
+      document.body.appendChild(socialIconLink);
+  
+      await socialIconLink.updateComplete;
+
+      img = socialIconLink.shadowRoot.querySelectorAll('a img');
+      link = socialIconLink.shadowRoot.querySelectorAll('a');
+    });
+
+    it('should have one <a> tag', () => {
+      expect(link.length).toBe(1);
+    });
+
+    it('should have default <a> tag href', () => {
+      expect(link[0].href).toContain(mockLink);
+    });
+
+    it('should have default <a> tag rel', () => {
+      expect(link[0].rel).toBe('noopener');
+    });
+
+    it('should have default <a> tag target', () => {
+      expect(link[0].target).toBe('_blank');
+    });
+
+    it('should have one <img> tag inside the <a> tag', () => {
+      expect(img.length).toBe(1);
+    });
+
+    it('should have default <img> tag with src', () => {
+      expect(img[0].src).toContain('/assets/logos/medium.svg');
+    });
+
+  });
+
 });
