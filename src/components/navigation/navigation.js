b3f1e17bca185f83b6c29bdbe0e135eea5314c4a
Owen Buckley <owenbuckley13@gmail.com>
2019-08-18 16:58:12 -0400

    navigation

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -2,15 +2,13 @@ class NavigationComponent extends HTMLElement {
   constructor() {
     super();
 
-    this.root = this.attachShadow({ mode: 'closed' });
+    this.root = this.attachShadow({ mode: 'open' });
   }
 
-  // run some code when the component is ready
   connectedCallback() {
     this.root.innerHTML = this.getTemplate();
   }
 
-  // create templates that interpolate variables and HTML!
   getTemplate() {
     return `
       <style>
