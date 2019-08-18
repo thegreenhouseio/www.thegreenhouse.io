a260b5bbd1ae8f95b3234a1b2feed7e91ebac20e
Owen Buckley <owenbuckley13@gmail.com>
2019-08-18 11:48:58 -0400

    footer spec

---
+++ src/components/blog-post/blog-post.spec.js
@@ -0,0 +1,212 @@
+// import './blog-post';
+
+// describe('BlogPost', () => {
+//   let header;
+
+//   beforeEach(async () => {
+//     header = document.createElement('app-header');
+
+//     document.body.appendChild(header);
+
+//     await header.updateComplete;
+//   });
+
+//   afterEach(() => {
+//     header.remove();
+//     header = null;
+//   });
+
+//   describe('Default Behavior', () => {
+//     it('should have a greeting', () => { 
+//       const greeting = header.shadowRoot.querySelectorAll('div.header h2')[0];
+
+//       expect(greeting.innerHTML).toMatch('A DREAMER BY DESIGN');
+//     });
+
+//     it('should have a logo', () => { 
+//       const logo = header.shadowRoot.querySelectorAll('.header a')[0];
+
+//       expect(logo.style).toBeDefined();
+//       expect(logo.title).toBeDefined();
+//       expect(1).toBe(1);
+//     });
+
+//     it('should have a link to the project website', () => { 
+//       const link = header.shadowRoot.querySelectorAll('.header a')[0];
+
+//       expect(link.href).toBe('https://www.greenwoodjs.io/');
+//     });
+//   });
+
+// });
+
+// import * as React from 'react';
+// import { mount, configure } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-15';
+// import Helmet from 'react-helmet';
+// import { slugifyDate } from './blog-post';
+// import BlogPost from './blog-post';
+
+// configure({ adapter: new Adapter() });
+
+// describe('BlogPost Component', () => {  
+  
+//   describe('slugifyDate', () => {
+//     const mockPost = {
+//       title: 'Some title for this post',
+//       date: '04.11.2018',
+//       image: 'image.png'
+//     };
+    
+//     it('should return a slugified date when the right format is provided', () => {
+//       const slugifiedDate = slugifyDate(mockPost.date);
+
+//       expect(slugifiedDate).toBe('2018/11/04/');
+//     });
+//   });
+
+//   describe('basic functionality', () => {
+//     const mockPost = {
+//       title: 'Some title for this post',
+//       date: '04.11.2018',
+//       image: 'image.png'
+//     };
+//     let post;
+
+//     beforeEach(() => {
+//       post = mount(
+//         <BlogPost {...mockPost}>
+//           <p>Hello World</p>
+//         </BlogPost>
+//       );
+//     });
+
+//     it('should not be null', () => {
+//       expect(post).not.toBeNull();
+//       expect(post.find('.blog-post').length).toEqual(1);
+//     });
+
+//     it('should display the correct title', () => {
+//       const title = post.find('h1.title');
+
+//       expect(title.length).toBe(1);
+//       expect(title.text()).toBe(mockPost.title);
+//     });
+
+//     it('should display the correct date', () => {
+//       const date = post.find('h5.date');
+
+//       expect(date.length).toBe(1);
+//       expect(date.text()).toBe(`Published: ${mockPost.date}`);
+//     });
+
+//     it('should display the correct image if the image path is local', () => {
+//       const header = post.find('.header');
+
+//       expect(header.length).toBe(1);
+//       expect(header.props().style.backgroundImage).toBe(`url(\'${mockPost.image}\')`);
+//     });
+
+//     it('should display the correct content', () => {
+//       const content = post.find('.content');
+
+//       expect(content.length).toBe(1);
+//       expect(content.text()).toBe('Hello World');
+//     });
+
+//     describe('<meta> tags for Social Sharing', () => {
+//       beforeEach(() => {
+//         post = mount(
+//           <BlogPost {...mockPost}>
+//             <p>Hello World</p>
+//           </BlogPost>
+//         );
+//       });
+  
+//       it('should have a <Helmet> component', () => {
+//         const helmet = post.find(Helmet);
+  
+//         expect(helmet.length).toBe(1);
+//       });
+  
+//       it('should have a <meta> tag for title', () => {
+//         const helmet = Helmet.peek();
+        
+//         helmet.metaTags.filter((tag) => {
+//           if (tag.property === 'og:title') {
+//             expect(tag.content).toBe('The Greenhouse I/O - Blog');
+//           }
+//         });
+//       });
+
+//       it('should have a <meta> tag for type', () => {
+//         const helmet = Helmet.peek();
+        
+//         helmet.metaTags.filter((tag) => {
+//           if (tag.property === 'og:type') {
+//             expect(tag.content).toBe('article');
+//           }
+//         });
+//       });
+
+//       it('should have a <meta> tag for url', () => {
+//         const helmet = Helmet.peek();
+//         const slugDate = slugifyDate(mockPost.date);
+
+//         helmet.metaTags.filter((tag) => {
+//           if (tag.property === 'og:url') {
+//             expect(tag.content).toBe(`https://www.thegreenhouse.io/blog/${slugDate}`);
+//           }
+//         });
+//       });
+
+//       it('should have a <meta> tag for image with a remote path', () => {
+//         const helmet = Helmet.peek();
+        
+//         helmet.metaTags.filter((tag) => {
+//           if (tag.property === 'og:image') {
+//             expect(tag.content).toBe(`https://s3.amazonaws.com/www.thegreenhouse.io${mockPost.image}`);
+//           }
+//         });
+//       });
+
+//       it('should have a <meta> tag for description', () => {
+//         const helmet = Helmet.peek();
+        
+//         helmet.metaTags.filter((tag) => {
+//           if (tag.property === 'og:description') {
+//             expect(tag.content).toBe(mockPost.title);
+//           }
+//         });
+//       });
+//     });
+//   });
+
+//   describe('Extended Meta Tag Functionality', () => {
+//     const mockPost = {
+//       title: 'Some title for this post',
+//       date: '04.11.2018',
+//       description: 'An optional description',
+//       image: 'https://s3.amazonaws.com/uploads.thegreenhouse.io/project-evergreen/logo-small.png'
+//     };
+
+//     beforeEach(() => {
+//       mount(
+//         <BlogPost {...mockPost}>
+//           <p>Hello World</p>
+//         </BlogPost>
+//       );
+//     });
+
+//     it('should have a <meta> tag for image with a remote URL / path when provided an external URL', () => {
+//       const helmet = Helmet.peek();
+      
+//       helmet.metaTags.filter((tag) => {
+//         if (tag.property === 'og:description') {
+//           expect(tag.content).toBe(mockPost.description);
+//         }
+//       });
+//     });
+//   });
+
+// });
---
+++ src/components/footer/footer.spec.js
@@ -0,0 +1,62 @@
+import './footer.js';
+
+describe('Footer Component', () => {
+  let footer;
+
+  beforeEach(async () => {
+    footer = document.createElement('app-footer');
+
+    document.body.appendChild(footer);
+
+    await footer.updateComplete;
+  });
+
+  afterEach(() => {
+    footer.remove();
+    footer = null;
+  });
+
+  describe('Default Behavior', () => {
+    it('should not be null', () => {
+      expect(footer).not.toBeNull();
+      expect(footer.shadowRoot.querySelectorAll('footer').length).toEqual(1);
+    });
+  
+    it('should have four <app-social-icon-link/> components in a list', () => {
+      expect(footer.shadowRoot.querySelectorAll('ul li app-social-icon-link').length).toEqual(4);
+    });
+  
+    it('should display copyright text', () => {
+      const copyright = footer.shadowRoot.querySelectorAll('.copyright')[0];
+
+      expect(copyright.innerHTML).toEqual('© Owen Buckley / thegreenhouse.io');
+    });
+
+    describe('social link icons', () => {
+      it('should have a linkedin icon', () => {
+        const linkedin = footer.shadowRoot.querySelectorAll('ul li app-social-icon-link[name="linkedin"]');
+       
+        expect(linkedin.length).toBe(1);
+      });
+
+      it('should have a twitter icon', () => {
+        const twitter = footer.shadowRoot.querySelectorAll('ul li app-social-icon-link[name="twitter"]');
+       
+        expect(twitter.length).toBe(1);
+      });
+
+      it('should have a medium icon', () => {
+        const medium = footer.shadowRoot.querySelectorAll('ul li app-social-icon-link[name="medium"]');
+       
+        expect(medium.length).toBe(1);
+      });
+
+      it('should have a github icon', () => {
+        const github = footer.shadowRoot.querySelectorAll('ul li app-social-icon-link[name="github"]');
+       
+        expect(github.length).toBe(1);
+      });
+    });
+  });
+
+});
