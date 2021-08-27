633e274de06bbe9bd1d934b7fee45d5e3435d2ee
Owen Buckley <owenbuckley13@gmail.com>
2021-08-27 17:56:07 -0400

    correct link detection logic for social links and add test case (#242)

--- src/components/social-icon-link/social-icon-link.spec.js
+++ src/components/social-icon-link/social-icon-link.spec.js
@@ -55,7 +55,7 @@ describe('SocialIconLink Component', () => {
     });
   });
 
-  describe('GitHub link', () => {
+  describe('GitHub.com link', () => {
     const mockLink = 'http://www.github.com/this';
     let img = '';
     let link = '';
@@ -99,7 +99,52 @@ describe('SocialIconLink Component', () => {
     it('should have <img> tag with github looo for alt', () => {
       expect(img[0].alt).contain('github logo');
     });
+  });
 
+  describe.only('GitHub.io link', () => {
+    const mockLink = 'http://projectevergreen.github.io/';
+    let img = '';
+    let link = '';
+
+    beforeEach(async () => {
+      socialIconLink = document.createElement('app-social-icon-link');
+      socialIconLink.link = mockLink;
+
+      document.body.appendChild(socialIconLink);
+  
+      await socialIconLink.updateComplete;
+
+      img = socialIconLink.shadowRoot.querySelectorAll('a img');
+      link = socialIconLink.shadowRoot.querySelectorAll('a');
+    });
+
+    it('should have one <a> tag', () => {
+      expect(link.length).to.equal(1);
+    });
+
+    it('should have default <a> tag href', () => {
+      expect(link[0].href).contain(mockLink);
+    });
+
+    it('should have default <a> tag rel', () => {
+      expect(link[0].rel).to.equal('noopener');
+    });
+
+    it('should have default <a> tag target', () => {
+      expect(link[0].target).to.equal('_blank');
+    });
+
+    it('should have <img> tag inside the <a> tag', () => {
+      expect(img.length).to.equal(1);
+    });
+
+    it('should have <img> tag with github logo for src', () => {
+      expect(img[0].src).contain('/assets/logos/github.svg');
+    });
+
+    it('should have <img> tag with github looo for alt', () => {
+      expect(img[0].alt).contain('github logo');
+    });
   });
 
   describe('Meetup link', () => {
