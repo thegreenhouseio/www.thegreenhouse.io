eef60e2ec86a657f677cb05cc9eaf0f4271592e8
Owen Buckley <owenbuckley13@gmail.com>
2021-12-21 12:40:37 -0500

    add contact page using Netlify forms (#256)
    * add contact page using Netlify forms
    
    * update navigation spec
    
    * add submit button
    
    * style submit button

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -21,6 +21,7 @@ class NavigationComponent extends LitElement {
             <li><a href="/about/">About</a></li>
             <li><a href="/projects/">Projects</a></li>
             <li><a href="/blog/">Blog</a></li>
+            <li><a href="/contact/">Contact</a></li>
           </ul>
         </nav>
       </div>
