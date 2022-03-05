92c9d59365c9d13380f16517de910fceaddf4add
Owen Buckley <owenbuckley13@gmail.com>
2022-03-05 13:18:10 -0500

    upgrade greenwood v0.24.0 and move content out of config (#267)

--- greenwood.config.js
+++ greenwood.config.js
@@ -2,27 +2,9 @@ import { greenwoodPluginGoogleAnalytics } from '@greenwood/plugin-google-analyti
 import { greenwoodPluginGraphQL } from '@greenwood/plugin-graphql';
 import { greenwoodPluginImportCss } from '@greenwood/plugin-import-css';
 import { greenwoodPluginPostCss } from '@greenwood/plugin-postcss';
-const DESCRIPTION = 'Personal site and blog for Owen Buckley and The Greenhouse I/O.  Ideas are built here.';
-const FAVICON_HREF = '/assets/favicon.ico';
-const TITLE = 'The Greenhouse I/O';
 
 export default {
   // TODO optimization: 'strict',
-
-  title: TITLE,
-
-  meta: [
-    { name: 'description', content: DESCRIPTION },
-    { name: 'twitter:creator', content: '@thegreenhouseio' },
-    { name: 'twitter:site', content: '@thegreenhouseio' },
-    { property: 'og:title', content: TITLE },
-    { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: 'https://www.thegreenhouse.io' },
-    { property: 'og:image', content: '/assets/og-meta-preview.png' },
-    { property: 'og:description', content: DESCRIPTION },
-    { rel: 'shortcut icon', href: FAVICON_HREF },
-    { rel: 'icon', href: FAVICON_HREF }
-  ],
 
   devServer: {
     port: 51201
